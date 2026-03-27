1e8a58269facc09b20ded1746d160684f4ffabda
souley <129006034+souleyFall@users.noreply.github.com>
2026-03-27 10:42:47 +0100

    Update users.controller.ts

--- src/user/users.controller.ts
+++ src/user/users.controller.ts
@@ -3,34 +3,32 @@ import { UsersService } from './users.service';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { JwtAuthGuard } from '../auth/jwt-auth.guard';
 import { RolesGuard } from '../auth/roles.guard';
+import { Roles } from '../auth/roles.decorator';
 
 @Controller('users')
+@UseGuards(JwtAuthGuard, RolesGuard) // Appliqué à tout le controller
 export class UsersController {
   constructor(private usersService: UsersService) {}
 
-  // ADMIN seulement
-  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles('ADMIN')
   @Get()
   findAll() {
     return this.usersService.findAll();
   }
 
-  // ADMIN seulement
-  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles('ADMIN')
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.usersService.findById(id);
   }
 
-  // ADMIN seulement
-  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles('ADMIN')
   @Patch(':id')
   update(@Param('id') id: string, @Body() dto: UpdateUserDto) {
     return this.usersService.update(id, dto);
   }
 
-  // ADMIN seulement
-  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles('ADMIN')
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.usersService.delete(id);
